680d7ad30ab6500f06b08aeedcf99f356d57c305
Tristan Harris <tris@mohc.net>
2011-09-23 10:46:27 +0100

    Modules

---
+++ 4.rb
@@ -0,0 +1,66 @@
+#Modules
+
+#Module as a function bag
+module Speach
+
+	def self.hello
+		'Hello'
+	end
+
+end
+
+puts 'Say hello: ' + Speach.hello
+puts
+
+
+#As a class mixin
+module Speach
+
+	def hi
+		'Hi'
+	end
+
+	def greet
+		"Hello, my name is #{name}"
+	end
+
+end
+
+class Person
+	include Speach
+
+	attr :name
+
+	def initialize(name)
+		@name = name
+	end
+
+end
+
+jeff = Person.new('Jeff')
+puts 'Jeff says hi: ' + jeff.hi
+puts 'Jeffs introduction: ' + jeff.greet
+puts
+
+#As an object extension
+obj = Object.new
+obj.extend Speach
+puts 'This object can speak: ' + obj.hi
+puts
+
+#Standard library mixins
+class Person
+	include Comparable
+
+	def <=>(other)
+		name.length <=> other.name.length
+	end
+
+end
+
+names = %w{Tom AlfonzoBongo George Fred}
+people = names.map{|name| Person.new(name)}
+puts 'Short people first'
+puts people.sort.inspect
+puts
+
